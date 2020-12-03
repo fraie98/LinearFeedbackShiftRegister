cdad151da88ba9c2e60ec5bbfc56cf25e38d0056
fraie98 <francesco.iemma11@gmail.com>
2020-12-03 21:31:14 +0100

    Implemented flip-flop and flip-flop with tap circuit. Updated doc (Architecture Description chapter started).

---
+++ LFSR/hdl/src/dff.vhd
@@ -0,0 +1,26 @@
+-- D Flip-Flop with Set/Reset
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity DFF is
+	port(
+			clk		:	in 	std_logic;
+			reset 	:	in 	std_logic;
+			set		: 	in 	std_logic;
+			d 		:	in 	std_logic;
+			q 		:	out std_logic)
+		);
+	end DFF;
+
+
+architecture rtl of DFF is
+begin
+	dff_p:process(reset,clk)
+	begin
+		if reset='0' then
+			q <= set;
+		elsif(rising_edge(clk)) then
+			q <= d;
+		end if;
+	end process;
+end rtl;
---
+++ LFSR/hdl/src/dff_tap_circuit.vhd
@@ -0,0 +1,42 @@
+-- D Flip-Flop with Set/Reset and tap circuit
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity DFF_tap_circuit is
+	port(
+			clk			:	in 	std_logic;
+			reset 		:	in 	std_logic;
+			set			: 	in 	std_logic;  -- initialization input
+			d_in		:	in 	std_logic;	
+			isTap		:	in 	std_logic;	-- it indicates if the input must be changed (it does if the previous ff is a tap)
+			feedback	:	in 	std_logic;  -- it is the feedback bit (the N-1)
+			q_out		:	out std_logic)
+		);
+	end DFF_tap_circuit;
+
+
+architecture rtl of DFF_tap_circuit is
+component dff is
+	port(
+			clk		:	in 	std_logic;
+			reset 	:	in 	std_logic;
+			set		: 	in 	std_logic;
+			d 		:	in 	std_logic;
+			q 		:	out std_logic)
+		);
+	end component;
+
+	signal new_d std_logic;
+begin
+	internal_dff:DFF
+		port map(
+				clk 	=> clock;
+				reset 	=> reset;
+				set 	=> set;
+				d 		=> new_d;
+				q 		=> q_out
+			);
+
+	new_d <= d_in xor (isTap and feedback);
+
+end rtl;

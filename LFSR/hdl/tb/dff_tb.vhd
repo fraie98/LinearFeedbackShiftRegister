e6c2195f3b41db012c1a43da074cfca3e328e146
fraie98 <francesco.iemma11@gmail.com>
2020-12-14 19:35:55 +0100

    Tests for dff and dff_tap_circuit performed and doc updated.

--- LFSR/hdl/tb/dff_tb.vhd
+++ LFSR/hdl/tb/dff_tb.vhd
@@ -1,10 +1,10 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity dff_tap_circuit_tb is
-end dff_tap_circuit_tb;
+entity dff_tb is
+end dff_tb;
 
-architecture bhv of dff_tap_circuit_tb is
+architecture bhv of dff_tb is
 	constant T_CLK   : time := 10 ns;
 	constant T_RESET : time := 25 ns;
 
@@ -14,20 +14,16 @@ architecture bhv of dff_tap_circuit_tb is
 	signal d_tb 		: std_logic;
 	signal q_tb 		: std_logic;
 	signal set_tb 		: std_logic := '0';
-	signal isTap_tb 	: std_logic;
-	signal feedback_tb	: std_logic;
 	signal end_sim 		: std_logic := '1';
 
 	-- COMPONENT
-	component dff_tap_circuit is
+	component dff is
 	port(
-			clk			:	in 	std_logic;
-			reset 		:	in 	std_logic;
-			set			: 	in 	std_logic;  
-			d_in		:	in 	std_logic;	
-			isTap		:	in 	std_logic;
-			feedback	:	in 	std_logic;  
-			q_out		:	out std_logic
+			clk		:	in 	std_logic;
+			reset 	:	in 	std_logic;
+			set 	:	in 	std_logic;
+			d 		:	in 	std_logic;
+			q 		:	out std_logic
 		);
 	end component;
 
@@ -35,15 +31,13 @@ begin
 	clk_tb <= (not(clk_tb) and end_sim) after T_CLK / 2;
 	reset_tb <= '1' after T_RESET;
 
-	test_dff : dff_tap_circuit
+	test_dff : dff
 	port map(
 			clk 		=> clk_tb,
 			reset  		=> reset_tb,
-			d_in 		=> d_tb,
-			q_out		=> q_tb,
-			set			=> set_tb,	
-			isTap 		=> isTap_tb,
-			feedback 	=> feedback_tb
+			d			=> d_tb,
+			q 			=> q_tb,
+			set			=> set_tb
 		);
 
 	dff_process : process(clk_tb,reset_tb)
@@ -51,18 +45,13 @@ begin
 	begin
 		if(reset_tb='0') then
 			t:=0;
-			isTap_tb <= '0'; 	-- It's a normal dff
-			feedback_tb <= '1';
 			d_tb <= '0';
 		elsif (rising_edge(clk_tb)) then
 			case(t) is
-				when 5  => d_tb <= '1';	
-				
-				when 10 => isTap_tb <= '1'; d_tb <='0';
+					
+				when 5 => d_tb <= '1';
 
-				when 15 => d_tb <='1';
-
-				when 20 => end_sim <= '0';
+				when 10 => end_sim <= '0';
 
 				when others => null;
 			

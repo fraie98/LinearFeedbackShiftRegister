fe0b5f45176cb7a32fbc798d28a917cf8182f5f0
fraie98 <francesco.iemma11@gmail.com>
2020-12-14 15:30:48 +0100

    Added script to check the differences between simulated hw lfsr and test program

--- lfsr_cpp/check.sh
+++ lfsr_cpp/check.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-diff output.txt ../LFSR/ModelSim/LFSR_test/fileout.tv
+# Check if there are differences between two file
+VAR=`diff output.txt ../LFSR/ModelSim/LFSR_test/fileout.tv`
+if [ -z "$VAR" ]
+then
+	echo "OK - NO DIFFERENCES"
+else
+	echo $VAR
+fi
+echo "Press a key to exit"
+read input

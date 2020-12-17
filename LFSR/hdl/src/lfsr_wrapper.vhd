dbf3afdad1b2d6358e97458c06524df7bb96e7de
fraie98 <francesco.iemma11@gmail.com>
2020-12-17 20:07:00 +0100

    Wrapper updated

--- LFSR/hdl/src/lfsr_wrapper.vhd
+++ LFSR/hdl/src/lfsr_wrapper.vhd
@@ -5,9 +5,9 @@ entity lfsr_wrapper is
 	port(
 			clk		:	in std_logic;
 			reset	:	in std_logic;	
-			seed	:	in std_logic_vector(3 downto 0);
-			led_out	:	out std_logic_vector(3 downto 0);
-			yq		:	out std_logic
+			seed	:	in std_logic_vector(0 to 3);
+			led_out	:	out std_logic_vector(0 to 3);
+			yq		:	out std_logic_vector(5 downto 0)
 		);
 end lfsr_wrapper;
 
@@ -16,7 +16,7 @@ architecture struct of lfsr_wrapper is
 signal seed_internal : std_logic_vector(0 to 15) := "0000101011000110";
 signal isTap_internal : std_logic_vector(0 to 14) := "000000000010110";
 signal state_internal : std_logic_vector(15 downto 0) := "----------------";
-
+signal out_internal :std_logic;
 
 component lfsr is
 	generic (Nbit : positive := 16);
@@ -34,7 +34,7 @@ begin
 	LFSR_MAPPING : lfsr
 	port map(
 			seed 		=> seed_internal,
-			outputBit	=> yq,
+			outputBit	=> out_internal,
 			reset		=> reset,
 			clock 		=> clk,
 			isTap 		=> isTap_internal,
@@ -44,5 +44,8 @@ begin
 	seed_internal(0 to 3) <= seed;
 	seed_internal(4 to 15) <= "000010101100";
   	
+	yq(0) <= out_internal; 
+	yq(5 downto 1) <= (others => '0');
+
   	led_out <= seed;
 end struct;
